941fc26ecdc63d6ebb7569a747032c0fdaa7c56e
Ken Narita <nariken930@yahoo.co.jp>
2017-12-13 14:25:57 +0900

    "no clutter delete image"

--- exam_rader.py
+++ exam_rader.py
@@ -15,7 +15,7 @@ def csv2npdata(filename):
     data = np.array(data)
     print("data_shape (row, col) : {}".format(data.shape) )
     data_num, data_samplenum = data.shape
-    
+
     return data, data_num, data_samplenum
 
 def clutter_data_extraction():
@@ -24,18 +24,18 @@ def clutter_data_extraction():
     x_range= []
     for i in range(data_samplenum):
         x_range.append(i * DELTA_R)
-        
+
     clutter_data = np.average(data, axis=0)
-    
+
     return x_range, clutter_data
-    
+
 def rader_spectrogram(filepath, dis_range, clutter_data):
     filename, ext = os.path.splitext( os.path.basename(filepath) )
     data, data_num, data_samplenum = csv2npdata(filepath)
-    
+
     sub_data = data - clutter_data
     abs_data = np.absolute(sub_data)
-    
+
     plt.imshow(abs_data.T, extent=[0, data_num * DELTA_T, 0, data_samplenum * DELTA_R], aspect="auto")
     plt.title("{}".format(filename + ext))
     plt.xlabel("times[s]")
@@ -43,15 +43,15 @@ def rader_spectrogram(filepath, dis_range, clutter_data):
     plt.colorbar()
     plt.savefig("./result/spec_{}.png".format(filename) )
     plt.show()
-    
+
 def main():
     print("delta_r[m] : {}\ndelta_t[s] : {}\ninitial_distance[m] : {}".format(DELTA_R, DELTA_T, INIT_DIS) )
-    
+
     dis_range, clutter_data = clutter_data_extraction()
-    
+
     file_list = glob.glob("./20171211/*.csv")
     for filepath in file_list:
         rader_spectrogram(filepath, dis_range, clutter_data)
-    
+
 if __name__=="__main__":
     main()
